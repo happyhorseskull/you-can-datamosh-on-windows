8b1376cc6adc691559ab1ded5fbfe5fb95107a33
happyhorseskull <amanicdroid@gmail.com>
2017-02-01 04:40:43 -0500

    Update video_to_gif.bat

--- video_to_gif.bat
+++ video_to_gif.bat
@@ -1,21 +1,23 @@
 @echo off
 
-                        rem    This assumes you want a GIF that's shorter than the entire video.  
-                        rem    Start time is how many seconds into the video to wait before starting to record the GIF
+                        rem    'start' is how many seconds into the video to wait before starting to record the GIF
                         rem    Set it to zero if you want to start at the beginning of the video
 
-                        rem    length sets how long the GIF will record. 
+                        rem    'length' sets how long the GIF will record. 
                         rem    If start is set to 4 and length set to 5 the GIF will record seconds 4 through 9 of the video
 set start=0
 set length=10
 
-                        rem keeps the current directory from changing if a file is dropped in the file explorer from a different folder
+                        rem    keeps the current directory from changing if a file is dropped in the file explorer from a different folder
 cd /d "%~dp0" 
 
+                        rem    If the spot for the video file name isn't blank then it jumps to the make_gif section
 if not [%1] == [] goto make_gif
 
-echo Please provide a video file name. Additionally you can include the second from the video that the GIF starts on and the GIF's length in seconds.
+                        rem    If a video file name wasn't given at the command line the message below is echoed and then the program quits
+echo Please provide a video file name. Additionally you can include the time in seconds from the video that the GIF starts on and the GIF's length in seconds.
 goto:eof
+
 
 :make_gif
 
@@ -26,6 +28,7 @@ goto:eof
   for %%f in (%1) do ( set "gif_name=%%~nf" )
   set gif_file="%gif_folder%\%gif_name%.gif"
   
+                        rem    Assigns default values at the start of the file if none given at command line
   if [%2] == [] ( 
     set start_time=%start%
   ) else (
@@ -50,9 +53,9 @@ goto:eof
 			                  rem    the stats_mode option can be either stats_mode=diff or stats_mode=full
                         rem    stats_mode=full chooses colors that will optimize colors for the entire frame 
                         rem    while stats_mode=diff optimizes colors to make the changes look good 
-  start /b /wait ffmpeg.exe -v warning -ss %start_time% -t %duration% -i %1 -vf "%filters%,palettegen=stats_mode=diff" -y %palette%
+  start /b /wait ffmpeg.exe -v error -i %1 -ss %start_time% -t %duration% -vf "%filters%,palettegen=stats_mode=diff" -y %palette%
   
                         rem    the second run uses the color palette while making the GIF
-  start /b /wait ffmpeg.exe -v warning -ss %start_time% -t %duration% -i %1 -i %palette% -lavfi "%filters% [x]; [x][1:v] paletteuse" -y %gif_file%
+  start /b /wait ffmpeg.exe -v error -i %1 -ss %start_time% -t %duration% -i %palette% -lavfi "%filters% [x]; [x][1:v] paletteuse" -y %gif_file%
 
   rm %palette%
