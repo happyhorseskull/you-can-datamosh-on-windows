56217623dc0741d694a17bc6480b29527729150d
happyhorseskull <amanicdroid@gmail.com>
2017-02-01 07:46:15 -0500

    Update video_to_gif.bat

--- video_to_gif.bat
+++ video_to_gif.bat
@@ -1,12 +1,14 @@
 @echo off
 
-                        rem    'start_second' is how many seconds into the video to wait before starting to record the GIF
+                        rem    'start_gif' is how many seconds into the video to wait before starting to copy the video
                         rem    Set it to zero if you want to start at the beginning of the video
+                        rem    'end_gif' is the second when the copying stops
 
-                        rem    'length' sets how long the GIF will record. 
-                        rem    If start is set to 4 and length set to 5 the GIF will record seconds 4 through 9 of the video
-set start_second=0
-set length=10
+set start_gif=0
+set end_gif=10
+                        rem    'fps' is frames per second and 'gif_width' is how many pixels wide the final GIF will be
+set fps = 15
+set gif_width = 480
 
                         rem    keeps the current directory from changing if a file is dropped in the file explorer from a different folder
 cd /d "%~dp0" 
@@ -15,7 +17,7 @@ cd /d "%~dp0"
 if not [%1] == [] goto make_gif
 
                         rem    If a video file name wasn't given at the command line the message below is echoed and then the program quits
-echo Please provide a video file name. Additionally you can include the time in seconds from the video that the GIF starts on and the GIF's length in seconds.
+echo Please provide a video file name. Additionally you can include the time in seconds from the video that the GIF starts and ends.
 goto:eof
 
 
@@ -30,23 +32,23 @@ goto:eof
   
                         rem    Assigns default values at the start of the file if none given at command line
   if [%2] == [] ( 
-    set start_time=%start_second%
+    set start_time = %start_second%
   ) else (
-    set start_time=%2
+    set start_time = %2
   )
                        
   if [%3] == [] (
-    set duration=%length%
+    set /A duration = %start_time% + %end_gif%
   ) else (
-    set duration=%3
+    set /A duration = %3 - %start_time%
   )
 
   echo Creating GIF, please wait.
   
                         rem    GIF making information adapted from http://blog.pkh.me/p/21-high-quality-gif-with-ffmpeg.html
   
-  set palette="%gif_folder%\palette.png"
-  set filters="fps=15,scale=480:-2:flags=lanczos"
+  set palette = "%gif_folder%\palette.png"
+  set filters = "fps=%fps,scale=%gif_width%:-2:flags=lanczos"
   
                         rem    the first run generates a global palette of 256 colors that will be used for every frame
                         
