6725c40b9822d6480477196a8cba9f4a69b62d1e
happyhorseskull <amanicdroid@gmail.com>
2017-01-31 23:41:37 -0500

    Update datamosh.bat
    fixed error that occurred when dropping a video from a different directory onto the bat file and it would change the current directory

--- datamosh.bat
+++ datamosh.bat
@@ -1,15 +1,12 @@
 @echo off
 
-rem keeps the current directory from changing if a video is dropped on the bat file  
+rem keeps the current directory from changing if a video is dropped on the bat file from the file explorer
 cd /d "%~dp0" 
 
 if "%1"=="" (
-
     echo Please provide a video file name.
     goto:eof
-
 ) else (
-	
     echo Processing, please wait.
     start /b /wait python-3.6.0\python.exe do_the_mosh_win.py "%1"
 )
