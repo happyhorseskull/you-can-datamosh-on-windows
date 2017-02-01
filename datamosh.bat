322fc3e15c4e2515c3ae1db9a55d3e225a3c06d4
happyhorseskull <amanicdroid@gmail.com>
2017-01-31 23:39:38 -0500

    Update datamosh.bat

--- datamosh.bat
+++ datamosh.bat
@@ -1,8 +1,15 @@
 @echo off
+
+rem keeps the current directory from changing if a video is dropped on the bat file  
+cd /d "%~dp0" 
+
 if "%1"=="" (
+
     echo Please provide a video file name.
     goto:eof
-  ) else (
+
+) else (
+	
     echo Processing, please wait.
-    start /b /wait python-3.6.0\Python.exe do_the_mosh_win.py %1
+    start /b /wait python-3.6.0\python.exe do_the_mosh_win.py "%1"
 )
