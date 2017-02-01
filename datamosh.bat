d814d3084e2bafdfb1da0f5862e4687bf0a7991a
happyhorseskull <amanicdroid@gmail.com>
2017-02-01 00:04:56 -0500

    Update datamosh.bat
    rearranged the if-else so the error that a video wasn't included is the else

--- datamosh.bat
+++ datamosh.bat
@@ -1,12 +1,11 @@
 @echo off
 
-rem keeps the current directory from changing if a video is dropped on the bat file from the file explorer
+rem keeps the current directory from changing if a video is dropped on the bat file  
 cd /d "%~dp0" 
 
-if "%1"=="" (
-    echo Please provide a video file name.
-    goto:eof
-) else (
+if not "%1"=="" (
     echo Processing, please wait.
-    start /b /wait python-3.6.0\python.exe do_the_mosh_win.py "%1"
+    start /b /wait python-3.6.0\python.exe do_the_mosh_win.py %1
+) else (
+    echo Please provide a video file name.
 )
