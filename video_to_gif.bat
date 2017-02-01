bcdc7ad16036caa96f14c09e94485ca499c43463
happyhorseskull <amanicdroid@gmail.com>
2017-02-01 01:07:29 -0500

    Update video_to_gif.bat
    somehow a bunch of tabs showed up in the code WHICH IS UNCOOL so I had to go fix that

--- video_to_gif.bat
+++ video_to_gif.bat
@@ -2,41 +2,38 @@
 
 cd /d "%~dp0" 
 
-if not [%1] equ [] goto make_gif
+if not [%1] equ [] goto MAKE_GIF
 
-echo Please provide a video file name.
-goto:eof
+  echo Please provide a video file name.
+  goto:eof
 
-:make_gif
-                    rem    GIF making information adapted from http://blog.pkh.me/p/21-high-quality-gif-with-ffmpeg.html
 
-	                  rem    This assumes you want a GIF that's shorter than the entire video.  
-	                  rem    Start time is how many seconds into the video to wait before starting to record the GIF
-	                  rem    Set it to zero if you want to start at the beginning of the video
-  set start_time=4 
-	                  rem    duration sets how long the GIF will record. 
-                    rem    If start_time is set to 4 and duration set to 5 the GIF will record seconds 4 through 9 of the video
-  set duration=5
+:MAKE_GIF
+                          rem    GIF making information adapted from http://blog.pkh.me/p/21-high-quality-gif-with-ffmpeg.html
+  set start_time=0 
+  set duration=10 
+                          rem    Start time is how many seconds into the video to wait before starting to record the GIF
+                          rem    Set it to zero if you want to start at the beginning of the video
+                          rem    duration sets how long the GIF will record. 
+                          rem    If start_time is set to 4 and duration set to 5 the GIF will record seconds 4 through 9 of the video
 
-	set "gif_folder=gifs"
+  set "gif_folder=gifs"
 	
-	if not exist %gif_folder% mkdir %gif_folder%
+  if not exist %gif_folder% mkdir %gif_folder%
     
-  for %%f in (%1) do ( 
-	  set "gif_name=%%~nf" 
-	)
+  for %%f in (%1) do ( set "gif_name=%%~nf" )
 
-	set gif_file="%gif_folder%\%gif_name%.gif"
+  set gif_file="%gif_folder%\%gif_name%.gif"
 	
   set palette="gifs\palette.png"
   set filters="fps=15,scale=480:-2:flags=lanczos"
 
-	echo Creating GIF, please wait.
+  echo Creating GIF, please wait.
 	
-	                    rem    the stats_mode option can be either stats_mode=diff or stats_mode=full
-	                    rem    stats_mode=full chooses colors that will optimize colors for the entire frame 
-                      rem    while stats_mode=diff optimizes colors to make the changes look good 
+                          rem    the stats_mode option can be either stats_mode=diff or stats_mode=full
+                          rem    stats_mode=full chooses colors that will optimize colors for the entire frame 
+                          rem    while stats_mode=diff optimizes colors to make the changes look good 
   start /b /wait ffmpeg.exe -v warning -ss %start_time% -t %duration% -i %1 -vf "%filters%,palettegen=stats_mode=diff" -y %palette%
   start /b /wait ffmpeg.exe -v warning -ss %start_time% -t %duration% -i %1 -i %palette% -lavfi "%filters% [x]; [x][1:v] paletteuse" -y %gif_file%
 
-	rm %palette%
+  rm %palette%
